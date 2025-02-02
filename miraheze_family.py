11c994dfd4b241c08e9d58532dcc75cdb73c4f1e
Tangyuan <tangyuan110821@163.com>
2025-02-02 10:06:30 +0000

    fix workflow

--- miraheze_family.py
+++ miraheze_family.py
@@ -7,6 +7,10 @@ class MirahezeFamily(family.Family):
         self.langs = {
             'zhwpwiki': 'https://zhwpwiki.miraheze.org/w/api.php',  # 站点的 API URL
         }
+    @classmethod
+    def __post_init__(cls):
+        # 在这里设置 family 的语言和 API 端点
+        cls.languages = cls.langs
 
 # 将该 family 注册到 pywikibot 中
 pywikibot.site._families['miraheze'] = MirahezeFamily()
